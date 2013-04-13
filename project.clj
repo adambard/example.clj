5824fb30bf825cdc8ca652e30419977acba6a00a
Adam <adam@adambard.com>
2013-04-12 23:21:39 -0700

    Updated project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject example "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A wee example project demonstrating the (very) basics of Compojure and Ring"
+  :url "http://adambard.com/blog/Getting-started-with-Clojure-web-apps/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
